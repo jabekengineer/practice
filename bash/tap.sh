b4188e4f37264e0e55ffa0486429c613e4c923cb
Jason Sebek <jabekengineer@gmail.com>
2025-10-08 13:40:53 -0400

    bash currentprocess, nonlogin, login shell tests

--- bash/tap.sh
+++ bash/tap.sh
@@ -12,8 +12,14 @@ is_equal() {
     actual=$1;
     expected=$2;
     if [ "$actual" == "$expected" ]; then
+        echo "Actual $actual"
+        echo "Expected $expected"
         pass
+        echo
     else
+        echo "Actual $actual"
+        echo "Expected $expected"
         fail
+        echo
     fi
 }
--- bash/tests/test_shell.sh
+++ bash/tests/test_shell.sh
@@ -7,14 +7,31 @@ source $CONCEPT;
 # 1.1 Identify the shell
 # invoke shell_type() function from concepts/shell
 # expect output to say bash
-test_shell_process_name () {
-    # should return output of
-    actual=$(shell_process_name)
-    echo "Actual: $actual"
-    # In VSCode, $SHELL environment variable is /bin/bash
+test_current_process_name () {
+    # should return output of ~/bash/tests/test_shell.sh
+    actual=$(current_process_name)
+    # In VSCode, $SHELL environment variable is /bin/bash while $0 is /usr/bin/bash
     # In WT::Ubuntu, $SHELL is /bin/bash
     expected=$0
 
+    is_equal "$actual" "$expected"
+}
+
+test_nonlogin_shell_process_name () {
+    actual=$(nonlogin_shell_process_name)
+    expected="bash"
+
+    is_equal "$actual" "$expected"
+}
+
+test_login_shell_process_name () {
+    echo "== Manual test =="
+    echo "In new windows terminal type echo \$0"
+    login_shell_process_name
+    echo -n "What did echo \$0 produce? "
+    read actual
+
+    expected="-bash"
     is_equal "$actual" "$expected"
 }
 
@@ -26,4 +43,6 @@ test_shell_process_name () {
 
 
 # main
-test_shell_process_name
+test_current_process_name
+test_nonlogin_shell_process_name
+test_login_shell_process_name
